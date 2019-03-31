639d7f1b1ca7f4554b65789b9e26703dcb910080
handanunal <handan.unal@yahoo.com>
2019-03-31 20:23:35 +0300

    test

---
+++ Chapter10/PolymorphismTest.java
@@ -0,0 +1,22 @@
+/**
+ * Figure 10.1 - PolymorphismTest
+ * 
+ * Author: Handan Unal
+ * 
+ */
+
+package figures.basics;
+
+public class PolymorphismTest {
+
+	public static void main(String[] args) {
+		
+	                                        
+		BasePlusCommisionEmployee basePlusCommisionEmployee = new BasePlusCommisionEmployee("Jane", "Doe", "111-11-11", 10000, .06,300);
+		
+		CommissionEmployee commissionEmployee2 =  basePlusCommisionEmployee;
+		System.out.println(commissionEmployee2.toString());
+
+	}                
+
+}
