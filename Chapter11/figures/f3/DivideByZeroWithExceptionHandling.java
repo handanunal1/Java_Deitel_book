7c58d263e04321bda654df4549fc2dccf042f5e9
handanunal <handan.unal@yahoo.com>
2019-04-10 21:12:51 +0300

    figures 11.3 exceptions

---
+++ Chapter11/figures/f3/DivideByZeroWithExceptionHandling.java
@@ -0,0 +1,54 @@
+/**
+ *Exercise 11.3 -   DivideByZeroWithExceptionHandling
+ * 
+ * Author: Handan Unal
+ * 
+ */
+package figures.f3;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class DivideByZeroWithExceptionHandling {
+
+	public static int quotient(int numerator, int denominator) throws ArithmeticException {
+		return numerator / denominator;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		boolean continueLoop = true; // determines if more input is needed
+
+		do {
+
+			try {
+
+				System.out.println("Please enter an integer numerator:");
+				int numerator = scanner.nextInt();
+				System.out.println("Please enter an integer denominator:");
+				int denominator = scanner.nextInt();
+
+				int result = quotient(numerator, denominator);
+
+				System.out.printf("%nResult: %d / %d = %d%n", numerator, denominator, result);
+				continueLoop = false; // input successful; end looping
+
+			} catch (InputMismatchException inputMismatchException) {
+				System.err.printf("%nException: %s%n", inputMismatchException);
+				scanner.nextLine(); // discard input so user can try again
+				System.out.printf("You must enter integers. Please try again.%n%n");
+			}
+
+			catch (ArithmeticException arithmeticException) {
+				System.err.printf("%nException: %s%n", arithmeticException);
+				System.out.printf("Zero is an invalid denominator. Please try again.%n%n");
+			}
+
+		}
+
+		while (continueLoop);
+
+	}
+
+}
