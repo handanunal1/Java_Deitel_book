75cf83f1e203b20054791071135a29fdd2fc76ad
handanunal <handan.unal@yahoo.com>
2019-04-24 22:36:31 +0300

    Figures 14.5

---
+++ chapter14/figures/f5/StringIndexMethods.java
@@ -0,0 +1,30 @@
+/**
+ *Figures 14.5 -  StringIndexMethods
+ * 
+ * Author: Handan Unal
+ * 
+ */
+package figures.f5;
+
+public class StringIndexMethods {
+
+	public static void main(String[] args) {
+		String letters = "abcdefghijklmabcdefghijklm";
+		System.out.printf("'c' is located at index %d%n", letters.indexOf('c'));
+		System.out.printf("'a' is located at index %d%n", letters.indexOf('a', 1));
+		System.out.printf("'$' is located at index %d%n", letters.indexOf('$'));
+
+		System.out.printf("Last 'c' is located at index %d%n", letters.lastIndexOf('c'));
+		System.out.printf("'a' is located at index %d%n", letters.lastIndexOf('a', 25));
+		System.out.printf("'$' is located at index %d%n", letters.lastIndexOf('$'));
+
+		System.out.printf("\"def\" is located at index %d%n", letters.indexOf("def"));
+		System.out.printf("\"def\" is located at index %d%n", letters.indexOf("def", 7));
+		System.out.printf("\"hello\" is located at index %d%n", letters.indexOf("hello"));
+
+		System.out.printf("\"def\" is last located at index %d%n", letters.lastIndexOf("def"));
+		System.out.printf("\"def\" is  last located at index %d%n", letters.lastIndexOf("def",25));
+		System.out.printf("\"hello\" is  last located at index %d%n", letters.lastIndexOf("hello"));
+	}
+
+}
