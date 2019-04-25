313a4d42b81781fb588d1163329b1e75a031d5e8
handanunal <handan.unal@yahoo.com>
2019-04-25 21:24:06 +0300

    Figures 14.7

---
+++ chapter14/figures/f7/StringConcatenation.java
@@ -0,0 +1,19 @@
+/**
+ *Figures 14.7 -  StringConcatenation
+ * 
+ * Author: Handan Unal
+ * 
+ */
+package figures.f7;
+
+public class StringConcatenation {
+
+	public static void main(String[] args) {
+	  String s1 = "Happy";
+	  String s2 = "Birthday";
+	  System.out.printf("s1 = %s%ns2 = %s%n%n",s1, s2); 
+	  System.out.printf( "Result of s1.concat(s2) = %s%n", s1.concat(s2));
+	  System.out.printf("s1 after concatenation = %s%n", s1); 
+	}
+
+}
