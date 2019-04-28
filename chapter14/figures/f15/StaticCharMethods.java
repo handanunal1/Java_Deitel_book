0158bd983b66d09fc12e29040bc3bbf3b4bd867d
handanunal <handan.unal@yahoo.com>
2019-04-28 14:05:19 +0300

    Figures 14.16 - character conversion

--- chapter14/figures/f15/StaticCharMethods.java
+++ chapter14/figures/f15/StaticCharMethods.java
@@ -23,8 +23,8 @@ public class StaticCharMethods {
 		System.out.printf("is letter or digit: %b%n", Character.isLetterOrDigit(c));
 		System.out.printf("is upper case: %b%n", Character.isUpperCase(c));
 		System.out.printf("is lower case: %b%n", Character.isLowerCase(c));
-		System.out.printf("to upper case: %b%n", Character.toUpperCase(c));
-		System.out.printf("to lower case: %b%n", Character.toLowerCase(c));
+		System.out.printf("to upper case: %s%n", Character.toUpperCase(c));
+		System.out.printf("to lower case: %s%n", Character.toLowerCase(c));
 	}
 
 }
---
+++ chapter14/figures/f16/StaticCharMethods2.java
@@ -0,0 +1,42 @@
+/**
+ *Figures 14.16 -   StaticCharMethods2 
+ * 
+ * Author: Handan Unal
+ * 
+ */
+package figures.f16;
+
+import java.util.Scanner;
+
+public class StaticCharMethods2 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Please enter a radix");
+
+		int radix = scanner.nextInt();
+
+		System.out.printf("Please choose one %n1 -- %s%n2 -- %s%n", "Convert digit to character",
+				"Convert character to digit");
+		int choice = scanner.nextInt();
+
+		switch (choice) {
+		case 1:
+			System.out.println("Enter a digit");
+			int digit = scanner.nextInt();
+			System.out.printf("Convert digit to character: %s%n", Character.forDigit(digit, radix));
+			break;
+		case 2:
+			char character = scanner.next().charAt(0);
+			System.out.println("Enter a character");
+			System.out.printf("Convert character to digit: %s%n ", Character.digit(character, radix));
+			break;
+		default:
+			break;
+		}
+
+	}
+
+}
