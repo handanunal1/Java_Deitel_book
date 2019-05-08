bced73e435bb1a1a1521a819767b451e801b48d8
handanunal <handan.unal@yahoo.com>
2019-05-08 22:06:38 +0300

    Exercises 14.15 14.16

---
+++ Chapter14/exercises/e18/TextAnalysis.java
@@ -0,0 +1,44 @@
+/**
+ *Exercises 14.18 -  TextAnalysis 
+ * 
+ * Author: Handan Unal
+ * 
+ */
+package exercises.e18;
+
+import java.util.Scanner;
+
+public class TextAnalysis {
+
+	public static void main(String[] args) {
+		System.out.println("Please give an input sentence; ");
+		Scanner scanner = new Scanner(System.in);
+		String text = scanner.nextLine();
+		int ascii = 0;
+		char character;
+		int[] arrayLetters = new int[26];
+		for (int i = 0; i < text.length(); i++) {
+
+			character = text.charAt(i);
+			ascii = (int) character;
+			if (ascii >= 97) {
+				arrayLetters[ascii - 97]++;
+			}
+
+		}
+
+		System.out.println("The statistics of letter used in the sentence above; ");
+
+		for (int i = 0; i < arrayLetters.length; i++) {
+
+			if (arrayLetters[i] > 0) {
+				character = (char) (i + 97);
+
+				System.out.println(character + " is used " + arrayLetters[i] + "times");
+			}
+
+		}
+
+	}
+
+}
