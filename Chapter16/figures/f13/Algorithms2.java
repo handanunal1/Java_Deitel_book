01e68a1c2975b54a6b2f1d2ac5fad4e625abd65a
handanunal <handan.unal@yahoo.com>
2019-06-16 19:04:20 +0300

    Exercises 16 - exercises

---
+++ Chapter16/figures/f13/Algorithms2.java
@@ -0,0 +1,47 @@
+package chapter16.figures.f13;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Figures 16.13 BinarySearchTest
+ * 
+ * Author: Handan Unal
+ * 
+ */
+public class Algorithms2 {
+
+	public static void main(String[] args) {
+		String[] colors = { "red", "white", "yellow", "blue" };
+		List<String> list1 = Arrays.asList(colors);
+		ArrayList<String> list2 = new ArrayList<>();
+		list2.add("black");
+		list2.add("red");
+		list2.add("green");
+		
+		System.out.println("Before adAll list2 contains: ");
+		
+		for(String s: list2) {
+			System.out.printf("%s ",s);
+		}
+		
+		Collections.addAll(list2,colors);
+		System.out.printf("%nAfter addAll, list2 contains: ");
+		
+		
+
+		for(String s: list2) {
+			System.out.printf("%s ",s);
+		}
+		
+		int frequency = Collections.frequency(list2, "red");
+		System.out.println("\nFrequency of red in list2 is: "+ frequency );
+		
+		boolean disjoint = Collections.disjoint(list1, list2);
+		System.out.printf("List 1 and list2 have elements in common\n",disjoint ? "do not have" : "have");
+				
+	}
+
+}
