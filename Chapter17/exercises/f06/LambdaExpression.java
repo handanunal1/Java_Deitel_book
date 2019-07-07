53f63ac0028fff94eb0e0d9844f2ab85b3b34717
handanunal <handan.unal@yahoo.com>
2019-07-07 17:51:34 +0300

    Chapter 17 - Exercises

--- Chapter17/exercises/f06/LambdaExpression.java
+++ Chapter17/exercises/f06/LambdaExpression.java
@@ -10,13 +10,21 @@ import java.util.stream.DoubleStream;
 
 
 public class LambdaExpression {
-
+	interface Sayable{  
+	    public String say();  
+	}  
 	public static void main(String[] args) {
+
 		double a = 23.4;
 		double b = 33.4;
 		double[] values = {2.0,3.0};
 		System.out.println("Sum  of double values  : " + DoubleStream.of(values).sum());
 		System.out.println("Sum  of double values  : " + DoubleStream.of(a,b).sum());
+		
+		  Sayable s=()->{  
+		        return "Welcome to lambdas!";  
+		    };  
+		    System.out.println(s.say());  
 	}
 
 }
