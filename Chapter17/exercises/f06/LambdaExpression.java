a2518637b7eba30b4e1af000bf40fb2c86c0a798
handanunal <handan.unal@yahoo.com>
2019-07-08 21:59:02 +0300

    Chapter 17 - Exercise 6

--- Chapter17/exercises/f06/LambdaExpression.java
+++ Chapter17/exercises/f06/LambdaExpression.java
@@ -6,25 +6,39 @@
  */
 package chapter17.exercises.f06;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 
-
 public class LambdaExpression {
-	interface Sayable{  
-	    public String say();  
-	}  
+	interface Sayable {
+		public String say();
+	}
+
 	public static void main(String[] args) {
 
 		double a = 23.4;
 		double b = 33.4;
-		double[] values = {2.0,3.0};
+		double[] values = { 2.0, 3.0 };
 		System.out.println("Sum  of double values  : " + DoubleStream.of(values).sum());
-		System.out.println("Sum  of double values  : " + DoubleStream.of(a,b).sum());
+		System.out.println("Sum  of double values  : " + DoubleStream.of(a, b).sum());
+
+		Sayable s = () -> {
+			return "Welcome to lambdas!";
+		};
+		System.out.println(s.say());
 		
-		  Sayable s=()->{  
-		        return "Welcome to lambdas!";  
-		    };  
-		    System.out.println(s.say());  
+		/* Constructor reference for ArrayList */
+		List<String> strList = Arrays.asList("1", "2", "3");
+		List<Integer> intList = strList.stream().map(Integer::new).collect(Collectors.toList());
+		
+		intList.forEach (e -> System.out.println(e) );
 	}
+
+
+	
+	
+
 
 }
