9ee2da57c443e6893a23fa6261b3db2cad8e50a0
handanunal <handan.unal@yahoo.com>
2019-07-16 22:43:05 +0300

    Chapter 17 - Exercise 12

---
+++ Chapter17/exercises/f12/RemovalDuplicateWord.java
@@ -0,0 +1,32 @@
+/**
+ * Exercise 17.12 s RemovalDuplicateWord
+ * 
+ * Author: Handan Unal
+ *  (the output is different)
+ * 
+ */
+package chapter17.exercises.f12;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+public class RemovalDuplicateWord {
+
+	public static void main(String[] args)  throws IOException {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please give to input for listing unique word in the input: ");
+		String sentenceInput = scanner.nextLine();
+		String []array = sentenceInput.split(" ");
+	
+		List<String> list = Arrays.asList(array);
+		 
+		// Get collection without duplicate i.e. distinct only
+		List<String> distinctElements = list.stream().distinct().collect(Collectors.toList());
+		 
+		//Let's verify distinct elements
+		System.out.println(distinctElements);
+	}
+
+}
