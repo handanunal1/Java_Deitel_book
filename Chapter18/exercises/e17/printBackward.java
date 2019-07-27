6d861d847c0b02a0f4d2f743977d3b3e0e4a7d5d
handanunal <handan.unal@yahoo.com>
2019-07-27 19:02:39 +0300

    Chapter 18 - Exercise 17

---
+++ Chapter18/exercises/e17/printBackward.java
@@ -0,0 +1,28 @@
+/**
+ * Exercise 18.16  PrintArray
+ * 
+ * Author: Handan Unal  
+ *
+ *
+ */
+package chapter18.exercises.e17;
+
+public class printBackward {
+
+	public static void main(String[] args) {
+		int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int a = array.length;
+		System.out.println("Array elements; ");
+		printArrayRecursive(array, a);
+	}
+
+	public static void printArrayRecursive(int[] array, int a) {
+
+		if (a == 0) {
+		return;
+		} else {
+			System.out.print(array[a-1]+ " ");
+			printArrayRecursive(array, a - 1);
+		}
+	}
+}
