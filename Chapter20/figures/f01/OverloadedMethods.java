fb1a27ddf0da16fe046191cd9626f01ca5da9733
handanunal <handan.unal@yahoo.com>
2019-08-06 20:51:38 +0300

    Chapter 20 - Figure 01

---
+++ Chapter20/figures/f01/OverloadedMethods.java
@@ -0,0 +1,43 @@
+/**
+ * Figure 20.05   OverloadedMethods
+ */
+package chapter20.figures.f01;
+
+public class OverloadedMethods {
+
+	public static void main(String[] args) {
+		Integer[] integerArray = { 1, 2, 3, 4, 5, 6 };
+		Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7 };
+		Character[] characterArray = { 'H', 'E', 'L', 'L', 'O' };
+
+		System.out.println("Array IntegerArray contains:");
+		printArray(integerArray);
+		System.out.println("Array DoubleArray contains:");
+		printArray(doubleArray);
+		System.out.println("Array CharacterArray contains:");
+		printArray(characterArray);
+	}
+
+	public static void printArray(Integer[] integerArray) {
+		for (Integer element : integerArray) {
+			System.out.print(element + " ");
+		}
+		System.out.println();
+	}
+
+	public static void printArray(Double[] doubleArray) {
+		for (Double element : doubleArray) {
+			System.out.print(element + " ");
+		}
+		System.out.println();
+	}
+
+	public static void printArray(Character[] characterArray) {
+		for (Character Element : characterArray) {
+			System.out.print(Element + " ");
+		}
+		System.out.println();
+
+	}
+
+}
