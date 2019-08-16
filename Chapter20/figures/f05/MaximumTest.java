94583c2806e7581446260e8cb9fa25a501c8f85a
handanunal <handan.unal@yahoo.com>
2019-08-16 21:11:35 +0300

    Chapter 20 - Figure 06

--- Chapter20/figures/f05/MaximumTest.java
+++ Chapter20/figures/f05/MaximumTest.java
@@ -13,7 +13,7 @@ public class MaximumTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.printf("Maximum of %d, %d and %d is %d%n%n", 3, 4, 5, maximum(3, 4, 5));
+		System.out.printf("Maximum of %d, %d and %d is %d%n%n", 3, 4, 5, maximumTest(3, 4, 5));
 		System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f%n%n", 6.6, 8.8, 7.7, maximum(6.6, 8.8, 7.7));
 		System.out.printf("Maximum of %s, %s and %s is %s%n", "pear", "apple", "orange",
 				maximum("pear", "apple", "orange"));
@@ -33,4 +33,19 @@ public class MaximumTest {
 
 	}
 
+//Erasure of maximum
+   public static Comparable maximumTest(Comparable x,Comparable y,Comparable z) {
+		Comparable max = x;
+		
+		
+		if (y.compareTo(max) > 0) {
+			max = y;
+		}
+		if (z.compareTo(max) > 0) {
+			max = z;
+		}
+		
+		return max;
+	}
+	
 }
