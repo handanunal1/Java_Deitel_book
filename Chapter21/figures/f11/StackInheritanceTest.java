affbb7bfa88592a25940c96819327ec7ef3c079a
handanunal <handan.unal@yahoo.com>
2019-08-21 20:54:30 +0300

    Chapter 21 figure 10,11

--- Chapter21/figures/f11/StackInheritanceTest.java
+++ Chapter21/figures/f11/StackInheritanceTest.java
@@ -1,10 +1,11 @@
 package chapter21.figures.f11;
 
+import chapter21.figures.f03.EmptyListException;
 import chapter21.figures.f10.StackInheritance;
 
 public class StackInheritanceTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws chapter21.figures.f10.EmptyListException {
 		StackInheritance<Integer> stack = new StackInheritance<Integer>();
 		stack.push(-1);
 		stack.print();
@@ -12,6 +13,17 @@ public class StackInheritanceTest {
 		stack.print();
 		stack.push(1);
 		stack.print();
-	}
 
+		try {
+			int removedItem = 0;
+
+			while (true) {
+				removedItem = stack.pop(); // use pop
+				System.out.println(removedItem);
+
+			}
+		} catch (EmptyListException ex) {
+			ex.printStackTrace();
+		}
+	}
 }
